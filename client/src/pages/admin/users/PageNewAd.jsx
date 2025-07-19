c93fb15d8b42afec407b2a70bb87c6e4cd0bfdf7
Rokas Imelinskas <Rokas.Imelinskas@gmail.com>
2025-07-19 13:26:33 +0300

    banned users can't post new ads

--- client/src/pages/admin/users/PageNewAd.jsx
+++ client/src/pages/admin/users/PageNewAd.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import { AdNewForm } from "../../../components/form/AdNew";
 import { AdminTitle } from "../../../components/page-title/AdminTitle";
+import { UserContext } from "../../../context/user/UserContext";
 
 export function PageNewAd() {
+  const { userIsBanned } = useContext(UserContext);
+
   return (
     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <AdminTitle title="New ad " />
-      <AdNewForm />
+      {userIsBanned ? <AdminTitle title="Banned users can't post new ads " /> : <AdminTitle title="New ad " />}
+      {!userIsBanned && <AdNewForm />}
     </main>
   );
 }
